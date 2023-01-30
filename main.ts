9f3292e7e4abbae965762bef2642b0ed5b1ad52d
Thammarate <122539714+Thammarate@users.noreply.github.com>
2023-01-30 08:57:51 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,28 @@
-OLED.init(128, 64)
-let Rounds = 0
-let P1 = 0
-let P2 = 0
+input.onGesture(Gesture.Shake, function () {
+    reset()
+})
+function reset () {
+    OLED.init(128, 64)
+    Rounds = 0
+    P1 = 0
+    P2 = 0
+    Ties = 0
+    OLED.writeStringNewLine("shall we play a game?")
+    basic.pause(2000)
+    OLED.clear()
+    OLED.writeStringNewLine("Player 1 Score:" + P1)
+    OLED.newLine()
+    OLED.writeStringNewLine("Player 2 Score:" + P2)
+    OLED.newLine()
+    OLED.writeStringNewLine("Ties:" + Ties)
+    OLED.newLine()
+    OLED.writeStringNewLine("Rounds:" + Rounds)
+}
 let Ties = 0
-OLED.writeStringNewLine("shall we play a game?")
-basic.pause(2000)
-OLED.clear()
-OLED.writeStringNewLine("Player 1 Score:" + P1)
-OLED.newLine()
-OLED.writeStringNewLine("Player 2 Score:" + P2)
-OLED.newLine()
-OLED.writeStringNewLine("Ties:" + Ties)
-OLED.newLine()
-OLED.writeStringNewLine("Rounds:" + Rounds)
+let P2 = 0
+let P1 = 0
+let Rounds = 0
+reset()
 basic.forever(function () {
 	
 })
